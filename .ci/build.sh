890cafa99b2fd320b99642d5ae4df5f1f8a7e292
John Vandenberg <jayvdb@gmail.com>
2017-12-17 16:22:36 +0700

    .coafile: Ignore coala in generated GCI files
    The GCI exported data contains 'coala',
    so it needs to be excluded from the relevant checks.
    
    Also ignore private/** and delete it regularly,
    as a preventative measure for accidental leaks.
    
    Related to https://github.com/coala/community/issues/3

--- .ci/build.sh
+++ .ci/build.sh
@@ -7,6 +7,7 @@ mkdir private _site public
 if [[ -n "$GCI_TOKEN" ]]; then
   python manage.py fetch_gci_task_data private
   python manage.py cleanse_gci_task_data private _site
+  rm -rf private/
 else
   python manage.py fetch_old_gci_task_data _site || true
 fi
