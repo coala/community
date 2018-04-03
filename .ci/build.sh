8bcbfbacbeab61d3d4fe7d1325059244cf694197
shrikrishna <15bcs044@smvdu.ac.in>
2018-04-03 13:13:30 +0530

    Import getorg data
    Closes https://github.com/coala/community/issues/18

--- .ci/build.sh
+++ .ci/build.sh
@@ -17,5 +17,6 @@ fi
 python manage.py migrate
 python manage.py import_contributors_data
 python manage.py import_openhub_data
+python manage.py import_geolocation
 python manage.py collectstatic --noinput
 python manage.py distill-local public --force
