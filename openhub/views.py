f294015547ec2c0e5844f6e44ed68e5fdbadb252
Kai Chen <kaichen120@gmail.com>
2018-11-01 20:44:19 -0700

    OpenHub: /openhub shows how to get OH_TOKEN if not set
    /openhub now shows a short message on how to get an OH_TOKEN
    if it's not set
    
    Closes https://github.com/coala/community/issues/201

--- openhub/views.py
+++ openhub/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 from openhub.models import PortfolioProject
+from openhub.oh_token import OH_TOKEN
 
 
 def index(request):
-    projects = PortfolioProject.objects.all()
-    args = {'projects': projects}
+    errors = []
+    projects = []
+    if OH_TOKEN is None:
+        errors.append("""
+        OH_TOKEN is not specified as an environment variable. <br/>
+        You can get an OpenHub token by signing up at
+        <a href="https://www.openhub.net">openhub.net</a>.
+        """)
+    else:
+        projects = PortfolioProject.objects.all()
+
+    args = {'projects': projects, 'errors': errors}
     return render(request, 'openhub.html', args)
