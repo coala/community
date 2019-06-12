5f86b09570f531388aff11f71d0cc4b275c1cf66
kvgarg <co16326.ccet@gmail.com>
2019-06-12 16:12:07 +0530

    gci/: Redesign gci students web-page
    The redesigned web-page displays the data
    in a better UI/UX form with some additional
    information to make it more interactive and
    attractive.
    
    Closes https://github.com/coala/community/issues/257

--- gci/views.py
+++ gci/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
 from datetime import datetime
 from calendar import timegm
-import logging
-import requests
 
+import logging
+
+from django.views.generic import TemplateView
+
+from community.views import get_header_and_footer
+from data.models import Contributor
 from .students import get_linked_students
-from .gitorg import get_logo
 from .task import get_tasks
 
 STUDENT_URL = (
@@ -15,75 +17,78 @@ STUDENT_URL = (
 )
 
 
-def index(request):
-    logger = logging.getLogger(__name__ + '.index')
-    try:
-        get_tasks()
-    except FileNotFoundError:
-        logger.info('GCI data not available')
-        s = ['GCI data not available']
-    else:
-        s = gci_overview()
+class GCIStudentsList(TemplateView):
+    template_name = 'gci_students.html'
 
-    return HttpResponse('\n'.join(s))
+    def get_data_updated_time(self):
+        return timegm(datetime.utcnow().utctimetuple())
 
+    def get_all_students(self):
+        """
+        Get all GCI students by filtering the tasks that are valid
+        :return: A List of all students dict having necessary information
+        about the student
+        """
+        logger = logging.getLogger(__name__ + '.gci_overview')
+        linked_students = list(get_linked_students())
+        data = {
+            'students': list(),
+            'error': None
+        }
+        if not linked_students:
+            error_message = 'No GCI students are linked'
+            logger.info(error_message)
+            data['error'] = error_message
+            return data
+        org_id = linked_students[0]['organization_id']
+        for student in linked_students:
+            student_id = student['id']
+            username = student['username']
+            contributors = Contributor.objects.filter(login=username)
+            if contributors:
+                contrib = contributors.first()
+                student['url'] = STUDENT_URL.format(org_id=org_id,
+                                                    student_id=student_id)
+                student['name'] = contrib.name
+                student['bio'] = contrib.bio
+                student['public_repos'] = contrib.public_repos
+                student['public_gists'] = contrib.public_gists
+                student['followers'] = contrib.followers
+                data['students'].append(student)
+            else:
+                logger.warning(f"GCI Student {username} doesn't exists!"
+                               f' Please check the username.')
+        return data
 
-def gci_overview():
-    logger = logging.getLogger(__name__ + '.gci_overview')
-    linked_students = list(get_linked_students())
-    if not linked_students:
-        logger.info('No GCI students are linked')
-        return ['No GCI students are linked']
+    def get_gci_tasks_and_students(self):
+        """
+        Get all GCI students by fetching the GCI tasks and the students
+        :return: A list of all GCI Students
+        """
+        logger = logging.getLogger(__name__ + '.index')
+        gci_students = {
+            'data': {},
+            'error': None
+        }
+        try:
+            get_tasks()
+        except FileNotFoundError:
+            logger.info('GCI data not available')
+            error_message = ('No GCI data is available. Please create a'
+                             ' tasks.yaml file having GCI tasks related'
+                             ' data in it.')
+            gci_students['error'] = error_message
+        else:
+            data = self.get_all_students()
+            if data['error']:
+                gci_students['error'] = data['error']
+            else:
+                gci_students['data'] = data['students']
+        return gci_students
 
-    org_id = linked_students[0]['organization_id']
-    org_name = linked_students[0]['organization_name']
-    s = []
-    s.append('<link rel="stylesheet" href="static/main.css">')
-
-    favicon = get_logo(org_name, 16)
-    with open('_site/favicon.png', 'wb') as favicon_file:
-        favicon_file.write(favicon)
-
-    org_logo = get_logo(org_name)
-    with open('_site/org_logo.png', 'wb') as org_logo_file:
-        org_logo_file.write(org_logo)
-
-    s.append('<link rel="shortcut icon" type="image/png" '
-             'href="static/favicon.png"/>')
-    s.append('<img src="static/org_logo.png" alt="'+org_name+'">')
-    s.append('<h2>Welcome</h2>')
-    s.append('Hello, world. You are at the {org_name} community GCI website.'
-             .format(org_name=org_name))
-    s.append('Students linked to %s issues:<ul class="students">' % org_name)
-    for student in linked_students:
-        student_id = student['id']
-        username = student['username']
-
-        r = requests.get('https://api.github.com/users/{}'.format(username))
-
-        if r.status_code == 404:
-            continue
-
-        student_url = STUDENT_URL.format(org_id=org_id,
-                                         student_id=student_id,
-                                         )
-        s.append('<li class="student">'
-                 'STUDENT ID: <a href="{student_url}">{student_id}</a><br />'
-                 '<div class="github-card" data-github="{username}" '
-                 'data-width="400" data-theme="default"></div>'
-                 .format(student_url=student_url, student_id=student_id,
-                         username=username))
-
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    s.append('</ul><i id="time" class="timestamp" data-time="{unix}">'
-             'Last updated: {timestamp} '
-             '(<span id="ago" class="timeago"></span>)</i>'
-             .format(unix=timegm(datetime.utcnow().utctimetuple()),
-                     timestamp=timestamp))
-
-    s.append('<script src="//cdn.jsdelivr.net/gh/lepture/github-cards@1.0.2'
-             '/jsdelivr/widget.js"></script>')
-    s.append('<script src="static/timeago.js"></script>')
-    s.append('<script>loadTimeElements()</script>')
-
-    return s
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = get_header_and_footer(context)
+        context['gci_students'] = self.get_gci_tasks_and_students()
+        context['updated_time'] = self.get_data_updated_time()
+        return context
