cafc7e64ca8a3baf67a9cf6be80770f3b54908f6
kvgarg <co16326.ccet@gmail.com>
2019-06-14 17:03:49 +0530

    meta-review/: Redesign the webpage
    The redesigned web-page provides a nice
    UI/UX design to the web-page with "search"
    feature.
    
    Closes https://github.com/coala/community/issues/259

--- static/js/contributors.js
+++ static/js/contributors.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var close_icon = $('.contributors-section .fa-close');
     var results_body = $('.search-results-tbody');
     var searched_keyword = null;
+    var contributors_card = $('.contributor-card');
 
     function appendChildren(element, username, el_result_value,
                             hide_all_contributors){
@@ -13,15 +14,25 @@ $(document).ready(function(){
                 var row_id = result_td.id;
                 var login = row_id.replace('td-', '');
                 $('.contributor-card').css('display', 'none');
-                $('[login=' + login +']').css('display', 'block');
+                var contrib_card = $('[login=' + login +']');
+                if(contrib_card.hasClass('meta-reviewer')){
+                    contrib_card.css('display', 'flex');
+                }
+                else{
+                    contrib_card.css('display', 'block');
+                }
                 $('.search-results').css('display', 'none');
             });
         }
         element.append(result_td);
     }
 
+    function match(search, element, attribute){
+        return $(element).attr(attribute).toLowerCase().indexOf(search) > -1;
+    }
+
     search_input.on('keypress keyup', function(){
-        searched_keyword = search_input.val();
+        searched_keyword = search_input.val().toLowerCase();
         if(searched_keyword === ''){
             $('.search-results').css('display', 'none');
             close_icon.css('display', 'none');
@@ -29,36 +40,45 @@ $(document).ready(function(){
         else {
             $('.search-results').css('display', 'block');
             close_icon.css('display', 'block');
-            var search_by_login = $('[login^=' + searched_keyword +']');
-            var search_by_name = $('[name^=' + searched_keyword +']');
+            var all_results = [];
+            contributors_card.filter(function () {
+                if(match(searched_keyword, this, 'login') ||
+                   match(searched_keyword, this, 'name')) {
+                    all_results.push($(this));
+                }
+            });
             var results_tbody_tr = $('.search-results-tbody tr');
             results_tbody_tr.remove();
-            if(search_by_login.length + search_by_name.length === 0 ){
+            if(all_results.length === 0 ){
                 appendChildren(results_body, null, 'No results found!', false);
             }
             else {
-                var all_results = search_by_login.add(search_by_name);
-                for(var contrib in all_results.get()){
-                    if(all_results[contrib]){
-                        var login = all_results[contrib].getAttribute('login');
-                        var name = all_results[contrib].getAttribute('name');
-                        var result_value = null;
-                        if(name){
-                            result_value = login + " (" + name + ")";
-                        }
-                        else {
-                            result_value = login;
-                        }
-                        appendChildren(results_body, login, result_value, true);
+                all_results.forEach(function (result) {
+                    var login = result.attr('login');
+                    var name = result.attr('name');
+                    var result_value = null;
+                    if(name){
+                        result_value = login + " (" + name + ")";
                     }
-                }
+                    else {
+                        result_value = login;
+                    }
+                    appendChildren(results_body, login, result_value, true);
+                });
             }
         }
     });
 
     close_icon.on('click', function(){
-        $('.contributor-card').css('display', 'block');
+        var all_contrib_cards = $('.contributor-card');
+        if(all_contrib_cards.hasClass('meta-reviewer')){
+            all_contrib_cards.css('display', 'flex');
+        }
+        else {
+            all_contrib_cards.css('display', 'block');
+        }
         close_icon.css('display', 'none');
         search_input.val(null);
+        $('.search-results').css('display', 'none');
     });
 });
