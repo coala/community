ab5e8606f2be80f6084f4b0c3b16eadcebb8a538
kvgarg <kggarg14@gmail.com>
2019-06-25 15:57:07 +0530

    tests/: Remove dependency of testusers'
    As in future, the data will be fetched
    from coala-webservices intead of @sks444
    repository (webservices.coala.io). So,
    to remove that dependency the tests have
    been modified accordingly.

--- data/tests/test_contrib_data.py
+++ data/tests/test_contrib_data.py
@@ -2,7 +2,9 @@ import requests_mock
 
 from django.test import TestCase
 
-from data.contrib_data import get_contrib_data
+from data.contrib_data import get_contrib_data, import_data
+from gamification.tests.test_management_commands import (
+    get_false_contributors_data)
 
 
 class GetContribDataTest(TestCase):
@@ -10,3 +12,7 @@ class GetContribDataTest(TestCase):
     def test_get_contrib_data(self):
         with requests_mock.Mocker():
             get_contrib_data()
+
+    def test_false_contributor_data(self):
+        for contrib in get_false_contributors_data():
+            import_data(contrib)
