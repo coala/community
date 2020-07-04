3a9f06c0ffb4292fc246645ced9c95bd440270a1
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2020-07-04 13:24:21 +0530

    Fix netlify build issues
    This fixes some of the netlify build issues
    also changes the default build env to xenial

--- .ci/build.sh
+++ .ci/build.sh
@@ -13,7 +13,8 @@ EXPORTED_DATA="static/tasks.yaml static/instances.yaml static/$META_REVIEW_DATA"
 
 ISSUES_JSON="issues.json"
 
-python manage.py fetch_deployed_data --allow-failure _site $EXPORTED_DATA
+python manage.py fetch_deployed_data _site $EXPORTED_DATA \
+                 --repo-name community --hoster github
 
 if [[ -n "$GCI_TOKEN" ]]; then
   python manage.py fetch_gci_task_data private
